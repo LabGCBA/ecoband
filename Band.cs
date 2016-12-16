7a6ea646f73fca16889413bf559aabd22afd60b1
Rita Zerrizuela <zeta@widcket.com>
2016-12-16 11:02:32 -0300

    Cleanup

---
+++ Band.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Plugin.BLE.Abstractions;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace EcoBand
+{
+	public class Band
+	{
+		public Band(IAdapter adapter)
+		{
+			Adapter = adapter;
+		}
+
+		protected readonly IAdapter Adapter;
+		protected const string DeviceIdKey = "DeviceIdNavigationKey";
+		protected const string ServiceIdKey = "ServiceIdNavigationKey";
+		protected const string CharacteristicIdKey = "CharacteristicIdNavigationKey";
+		protected const string DescriptorIdKey = "DescriptorIdNavigationKey";
+	}
+}
---
+++ Service.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace EcoBand
+{
+	public class Service
+	{
+		public Service(String Label, String Uuint)
+		{
+			label = Label;
+			uuint = Uuint;
+		}
+
+		public String label { get; }
+		public String uuint { get; }
+	}
+}
