aa0b470c1a71b972e9e3c0504e7072018351a90d
Rita Zerrizuela <zeta@widcket.com>
2016-12-16 11:48:29 -0300

    Locked screen orientation

--- Main.cs
+++ Main.cs
@@ -1,37 +1,35 @@
 using System;
 
 using Android.App;
-using Android.Content;
-using Android.Runtime;
-using Android.Views;
+// using Android.Content;
+// using Android.Runtime;
+// using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Content.PM;
 
-namespace EcoBand
-{
-    [Activity (Label = "EcoBand", MainLauncher = true)]
-    public class Main : Activity
-    {
+namespace EcoBand {
+    [Activity(Label = "EcoBand", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait)]
+
+    public class Main : Activity {
         int count = 1;
 
-        protected override void OnCreate (Bundle bundle)
-        {
-            base.OnCreate (bundle);
+        protected override void OnCreate(Bundle bundle) {
+            base.OnCreate(bundle);
 
-			// LayoutInflater inflater = Application.Context.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
-			// View layout = inflater.Inflate(Resource.Layout.Main, null);
+            // LayoutInflater inflater = Application.Context.GetSystemService(Context.LayoutInflaterService) as LayoutInflater;
+            // View layout = inflater.Inflate(Resource.Layout.Main, null);
 
-			// Set our view from the "main" layout resource
-			SetContentView (Resource.Layout.Main);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
 
             // Get our button from the layout resource,
             // and attach an event to it
-            Button button = FindViewById<Button> (Resource.Id.btnConnect);
-            
+            Button button = FindViewById<Button>(Resource.Id.btnConnect);
+
             button.Click += delegate {
-                button.Text = string.Format("Thanks! {0} clicks.", count++); };
+                button.Text = string.Format("Thanks! {0} clicks.", count++);
+            };
         }
     }
 }
-
-
